53d23aed1d818e26a4ff35d4eb0e2299b5dad32b
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-04-15 14:56:50 +0900

    push_swap追加

---
+++ push_swap/checker.c
@@ -0,0 +1,11 @@
+#include "push_swap.h"
+
+int checker(int fd)
+{
+
+}
+
+int	main(int ac, char **av)
+{
+	return (0);
+}
---
+++ push_swap/push_swap.c
@@ -0,0 +1,14 @@
+#include "push_swap.h"
+
+//マージソートを用いる
+//swap and write
+void push_swap(int x)
+{
+
+}
+
+int main(int ac, char *av[])
+{
+printf("hello, world\n");
+return 0;
+}
