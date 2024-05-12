6946ec17ce0000f5f21c4905b8609d51d8c994fb
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-05-12 13:06:56 +0900

    pipex 5/12

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -19,18 +19,24 @@ void    hakanaiyume(char **av, char **envp, int firing_order, int output_file)
 	if (pipe(end) == -1)
 		return;
 	if (output_file != -42)
+	{
+		printf("%d 最後のコマンド\n", firing_order);
 		dup2(output_file, STDOUT_FILENO);
+		printf("output に記載される!!\n");
+	}
 	pid = fork();
     if (pid < 0)
         return;
     else if (pid == 0)
     {
+		printf("%d child\n", firing_order);
         //child
         close(end[0]);
         dup2(end[1], STDOUT_FILENO);
     }
     else if (pid > 0)
     {
+		printf("%d parent\n", firing_order);
         //parent
         close(end[1]);
         dup2(end[0], STDIN_FILENO);
@@ -50,11 +56,11 @@ int main(int ac, char **av, char **envp)
 	int	input_file;
 	int	output_file;
 
-	try(5);
+	// try(5);
     if (ac < 5)
         return 0;
     input_file = open(av[1], O_RDONLY);
-    output_file = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    output_file = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	dup2(input_file, STDIN_FILENO);
 
 	//発火!!!!
