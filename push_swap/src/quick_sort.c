b07df22beaa241419137acb5a1c8bf9768570842
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-05-19 12:10:51 +0900

    あまりもの

--- push_swap/src/quick_sort.c
+++ push_swap/src/quick_sort.c
@@ -1,141 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   quick_sort.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kkomatsu <kkomatsu@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/03 19:32:51 by kkomatsu          #+#    #+#             */
-/*   Updated: 2024/05/06 03:26:33 by kkomatsu         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../includes/push_swap.h"
 
-void	three_arg(char **av, t_dst **a, t_dst **b)
+int	find_limit(int len)
 {
-	long	top;
-	long	middle;
-	long	bottom;
+	int	limit;
 
-	top = ft_atol(av[0]);
-	middle = ft_atol(av[1]);
-	bottom = ft_atol(av[2]);
-	if (top < middle && middle > bottom && top < bottom)
+	limit = 0;
+	while ((len / 2) != 0)
 	{
-		pa(a, b);
-		sa(a);
-		pb(a, b);
+		limit++;
+		len = len / 2;
 	}
-	else if (top > middle && middle < bottom && top < bottom)
-		sa(a);
-	else if (top < middle && middle > bottom && top > bottom)
-		rra(a);
-	else if (top > middle && middle < bottom && top > bottom)
-		ra(a);
-	else if (top > middle && middle > bottom && top > bottom)
-	{
-		sa(a);
-		rra(a);
-	}
-	return ;
+	return (limit);
 }
 
-/*
-return (0); no swap
-return (1); swap a
-return (2); swap b
-return (3); swap a and b
-*/
-int	is_swap(t_dst **a, t_dst **b)
+int	all_quick(t_dst **a, t_dst **b)
 {
-	int		a_flag;
-	int		b_flag;
-	t_dst	*a_ptr;
-	t_dst	*b_ptr;
+    int size;
 
-	a_flag = 0;
-	b_flag = 0;
-	a_ptr = *a;
-	b_ptr = *b;
-	if (a_ptr->value > a_ptr->next->value && a_ptr != NULL
-		&& a_ptr->next != NULL)
-		a_flag = 1;
-	if (!(!(b_ptr->next)))
-		if (b_ptr->value < b_ptr->next->value && b_ptr != NULL
-			&& b_ptr->next != NULL)
-			b_flag = 1;
-	if (a_flag && b_flag)
-		return (3);
-	else if (a_flag)
-		return (1);
-	else if (b_flag)
-		return (2);
-	else
-		return (0);
+    size = find_limit(ft_lstsize(a));
+    while (size >= 0)
+    {
+
+        size--;
+    }
+    
+
 }
-
-/*
-return (0); no rotate
-return (1); rotate a
-return (2); rotate b
-return (3); rotate a and b
-*/
-int	is_rotate(t_dst **a, t_dst **b)
-{
-	int	a_flag;
-	int	b_flag;
-
-	a_flag = 0;
-	b_flag = 0;
-	if (!is_sorted_ascending(a))
-		a_flag = 1;
-	if (!is_sorted_descending(b))
-		b_flag = 1;
-	if (a_flag && b_flag)
-		return (3);
-	else if (a_flag)
-		return (1);
-	else if (b_flag)
-		return (2);
-	else
-		return (0);
-}
-
-void	let_go(t_dst **a, t_dst **b, int is_swap, int is_rotate)
-{
-	int	is_sort_a;
-	int	is_sort_b;
-
-	if (is_swap == 1)
-		sa(a);
-	else if (is_swap == 2)
-		sb(b);
-	else if (is_swap == 3)
-		ss(a, b);
-	is_sort_a = is_sorted_ascending(a);
-	is_sort_b = is_sorted_descending(b);
-	if (is_rotate == 1 && !is_sort_a)
-		rra(a);
-	else if (is_rotate == 2 && !is_sort_b)
-		rrb(b);
-	else if (is_rotate == 3 && !is_sort_a && !is_sort_b)
-		rrr(a, b);
-}
-
-int	alg_1(t_dst **a, t_dst **b)
-{
-	while (1)
-	{
-		let_go(a, b, is_swap(a, b), is_rotate(a, b));
-		if (is_sorted_ascending(a) && is_sorted_descending(b))
-			break ;
-	}
-	return (1);
-}
-
-// int	main(int ac, char **av)
-// {
-// 	printf("%ld \n", get_pivot(av));
-// 	return (0);
-// }
