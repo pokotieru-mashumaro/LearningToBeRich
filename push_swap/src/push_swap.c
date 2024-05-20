9b5eb5927b3b0cf0ce8e138a8207a421f42f426f
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-05-21 01:29:49 +0900

    aaa

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: komatsukotarou <komatsukotarou@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:47:56 by kkomatsu          #+#    #+#             */
-/*   Updated: 2024/05/21 01:10:19 by komatsukota      ###   ########.fr       */
+/*   Updated: 2024/05/21 01:27:16 by komatsukota      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,40 +77,40 @@ int	under_five_av_checker(t_dst **a, t_dst **b, char **av, t_config *conf)
 //     system("leaks -q a.out");
 // }
 
-int	main(int ac, char **av)
-{
-	t_dst	**a;
-	t_dst	**b;
-	t_config * conf;
+// int	main(int ac, char **av)
+// {
+// 	t_dst	**a;
+// 	t_dst	**b;
+// 	t_config * conf;
 
-	if (ac <= 2)
-		return (output_error());
-	skip_zeo(av);
-	if (is_av_error(av))
-		return (output_error());
-	a = (t_dst **)malloc(sizeof(t_dst *));
-	if (!a)
-		return (0);
-	b = (t_dst **)malloc(sizeof(t_dst *));
-	if (!b)
-	{
-		free(a);
-		return (0);
-	}
-	conf = (t_config *)malloc(sizeof(t_config));
-	init_dst(a, b, av, conf);
-	if (is_sorted_ascending(a))
-	{
-		ft_lstclear(a);
-		ft_lstclear(b);
-		return (0);
-	}
-	if (under_five_av_checker(a, b, av, conf))
-		return (0);
-	separated_by_pivot(av, a, b, get_pivot_num(av));
-	alg_1(a, b);
-	many_pb(a, b);
-	ft_lstclear(a);
-	ft_lstclear(b);
-	return (0);
-}
+// 	if (ac <= 2)
+// 		return (output_error());
+// 	skip_zeo(av);
+// 	if (is_av_error(av))
+// 		return (output_error());
+// 	a = (t_dst **)malloc(sizeof(t_dst *));
+// 	if (!a)
+// 		return (0);
+// 	b = (t_dst **)malloc(sizeof(t_dst *));
+// 	if (!b)
+// 	{
+// 		free(a);
+// 		return (0);
+// 	}
+// 	conf = (t_config *)malloc(sizeof(t_config));
+// 	init_dst(a, b, av, conf);
+// 	if (is_sorted_ascending(a))
+// 	{
+// 		ft_lstclear(a);
+// 		ft_lstclear(b);
+// 		return (0);
+// 	}
+// 	if (under_five_av_checker(a, b, av, conf))
+// 		return (0);
+// 	separated_by_pivot(av, a, b, get_pivot_num(av));
+// 	alg_1(a, b);
+// 	many_pb(a, b);
+// 	ft_lstclear(a);
+// 	ft_lstclear(b);
+// 	return (0);
+// }
