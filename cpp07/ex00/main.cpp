624342f044804092d93aa73cd4b9b038208888aa
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-11-08 10:46:22 +0900

    ex00 done

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -10,26 +10,6 @@
 
 #include "whatever.hpp"
 
-template <typename T>
-void swap(T& x, T& y)
-{
-    T temp = x;
-    x = y;
-    y = temp;
-}
-
-template <typename T>
-T min(T& x, T& y)
-{
-    return (x > y) ? y : x;
-}
-
-template <typename T>
-T max(T& x, T& y)
-{
-    return (x > y) ? x : y;
-}
-
 int main(void)
 {
     int a = 2;
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -3,8 +3,24 @@
 
 #include <iostream>
 
-template <typename T> void swap(T& x, T& y);
-template <typename T> T min(T& x, T& y);
-template <typename T> T max(T& x, T& y);
+template <typename T>
+void swap(T& x, T& y)
+{
+    T temp = x;
+    x = y;
+    y = temp;
+}
+
+template <typename T>
+T min(T& x, T& y)
+{
+    return (x > y) ? y : x;
+}
+
+template <typename T>
+T max(T& x, T& y)
+{
+    return (x > y) ? x : y;
+}
 
 #endif
