a5226c26de76c0947803ba0903bd063ac48a9e84
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-12-24 20:10:56 +0900

    Update changes

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,77 +1,180 @@
 #include "ScalarConverter.hpp"
 
+bool is_char(const std::string &input);
+bool is_int(const std::string &input);
+bool is_float(const std::string &input);
+bool is_double(const std::string &input);
+bool is_pseudo(const std::string &input);
+
+bool is_number(const std::string &input);
+int dot_count(const std::string &input);
 std::string get_integer(const std::string& input);
 
+int type_check(const std::string &input);
+
 void convert_char(const std::string& input);
-void convert_int(const std::string& input);
-void convert_float(const std::string& input);
-void convert_double(const std::string& input);
 
-bool is_impossible(const std::string& input);
-bool is_pseudo(const std::string& input);
-bool is_overflow(const std::string& input);
-
-void output_result(const std::string& input);
-void output_result_impossible();
-void output_result_pseudo(const std::string& pseudo);
+void output_char(const std::string &input);
+void output_int(const std::string &input);
+void output_float(const std::string &input);
+void output_double(const std::string &input);
+void output_psedo(const std::string& input);
+void output_impossible();
 
 ScalarConverter::ScalarConverter()
-{}
+{
+}
 
 ScalarConverter::ScalarConverter(const ScalarConverter &copy)
 {
-    *this = copy;
+	*this = copy;
 }
 
 ScalarConverter::~ScalarConverter()
-{}
+{
+}
 
 ScalarConverter &ScalarConverter::operator=(const ScalarConverter &copy)
 {
-    if (this != &copy)
-    {}
-    return *this;
+	if (this != &copy)
+	{
+	}
+	return *this;
 }
 
-void ScalarConverter::convert(const std::string& input)
+void ScalarConverter::convert(const std::string &input)
 {
-	if (is_pseudo(input))
-		return output_result_pseudo(input);
-	if (is_impossible(input))
-		return output_result_impossible();
-	output_result(input);
+	int type = type_check(input);
+
+	switch (type)
+	{
+	case TYPE_CHAR:
+		output_char(input);
+		break;
+	case TYPE_INT:
+		output_int(input);
+		break;
+	case TYPE_FLOAT:
+		output_float(input);
+		break;
+	case TYPE_DOUBLE:
+		output_double(input);
+		break;
+	case TYPE_PSEUDO:
+		output_psedo(input);
+		break;
+	default:
+		output_impossible();
+		break;
+	}
 }
 
-void output_result(const std::string& input)
+int type_check(const std::string &input)
 {
+	if (is_char(input))
+		return TYPE_CHAR;
+	else if (is_int(input))
+		return TYPE_INT;
+	else if (is_float(input))
+		return TYPE_FLOAT;
+	else if (is_double(input))
+		return TYPE_DOUBLE;
+	else if (is_pseudo(input))
+		return TYPE_PSEUDO;
+	return TYPE_IMPOSSIBLE;
+}
+
+void output_char(const std::string &input)
+{
+	int num = static_cast<int>(input[0]);
+
+	std::cout << "char: " << input[0] << std::endl;
+	std::cout << "int: " << num << std::endl;
+    std::cout << "float: " << num << ".0f" << std::endl;
+    std::cout << "double: " << num << ".0" << std::endl;
+}
+
+void output_int(const std::string &input)
+{
+    long long check_num = atoll(input.c_str());
+    if (check_num > INT_MAX || check_num < INT_MIN)
+    {
+        output_impossible();
+        return;
+    }
+    int num = static_cast<int>(check_num);
+
+    convert_char(input);
+    std::cout << "int: " << num << std::endl;
+    std::cout << "float: " << num << ".0f" << std::endl;
+    std::cout << "double: " << num << ".0" << std::endl;
+}
+
+void output_float(const std::string &input)
+{
+    double num = atof(input.c_str());
+    long num2 = atol(input.c_str());
+
+    convert_char(input);
+    
+    if (num2 > INT_MAX || num2 < INT_MIN)
+        std::cout << "int: impossible" << std::endl;
+    else
+        std::cout << "int: " << num2 << std::endl;
+
+	if (num > FLT_MAX || num < -FLT_MAX)
+	{
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
+	}
+	else
+	{
+		std::cout << "float: " << num << "f" << std::endl;
+    	std::cout << "double: " << num << std::endl;
+	}
+}
+
+void output_double(const std::string &input)
+{
+	double num = atof(input.c_str());
+	long num2 = atol(input.c_str());
+
 	convert_char(input);
-	convert_int(input);
-	convert_float(input);
-	convert_double(input);
+	
+	if (num2 > INT_MAX || num2 < INT_MIN)
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << num2 << std::endl;
+	
+	if (num > FLT_MAX || num < -FLT_MAX)
+		std::cout << "float: impossible" << std::endl;
+	else
+		std::cout << "float: " << num << "f" << std::endl;
+	std::cout << "double: " << num << std::endl;
 }
 
-void output_result_impossible()
-{
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: impossible" << std::endl;
-	std::cout << "float: impossible" << std::endl;
-	std::cout << "double: impossible" << std::endl;
-}
-
-void output_result_pseudo(const std::string& pseudo)
+void output_psedo(const std::string& input)
 {
 	std::string str;
 
-	if (pseudo == "nan" || pseudo == "nanf")
+	if (input == "nan" || input == "nanf")
 		str = "nan";
-	else if (pseudo == "-inf" || pseudo == "-inff")
+	else if (input == "-inf" || input == "-inff")
 		str = "-inf";
-	else if (pseudo == "+inf" || pseudo == "+inff")
+	else if (input == "+inf" || input == "+inff" || input == "inf" || input == "inff")
 		str = "+inf";
 	std::cout << "char: impossible" << std::endl;
 	std::cout << "int: impossible" << std::endl;
 	std::cout << "float: " << str << "f" << std::endl;
 	std::cout << "double: " << str << std::endl;
+}
+
+void output_impossible()
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: impossible" << std::endl;
+	std::cout << "double: impossible" << std::endl;
 }
 
 void convert_char(const std::string& input)
@@ -81,77 +184,79 @@ void convert_char(const std::string& input)
 
 	if (input_integer < 0 || input_integer > 255)
 		std::cout << "char: impossible" << std::endl;
-	if (input_integer < 32 || input_integer > 126)
+	else if (input_integer < 32 || input_integer > 126)
 		std::cout << "char: Non displayable" << std::endl;
 	else
 		std::cout << "char: '" << c << "'" << std::endl;
 }
 
-void convert_int(const std::string& input)
+bool is_char(const std::string &input)
 {
-	if (is_overflow(input))
-		std::cout << "int: impossible" << std::endl;
-	else
-	{
-		int input_integer = atoi(get_integer(input).c_str());
-		std::cout << "int: " << input_integer << std::endl;
-	}
-}
-
-void convert_float(const std::string& input)
-{
-	float cast = atof(input.c_str());
-	std::cout << "float: " << static_cast<double>(cast) << "f" << std::endl;
-}
-
-void convert_double(const std::string& input)
-{
-	float cast = atof(input.c_str());
-	std::cout << "double: " << static_cast<double>(cast) << std::endl;
-}
-
-bool is_impossible(const std::string& input)
-{
-	int f_count = 0;
-	int dot_count = 0;
-	int len = input.size();
-
-	if (input == "")
-		return true;
-	if (input[0] != '-' && (input[0] < '0' || input[0] > '9'))
-		return true;
-	for (int i = 1; i < len; i++)
-	{
-		char c = input[i];
-		if (c < '0' || c > '9')
-		{
-			if (c == 'f')
-				f_count++;
-			else if (c == '.')
-			{
-				if (f_count == 1)
-					return true;
-				else
-					dot_count++;
-			}
-			else
-				return true;
-		}
-	}
-	if (f_count > 1 || dot_count > 1)
-		return true;
-	if (f_count == 1 && input.back() != 'f')
-		return true;
-	if (dot_count == 1 && input.back() == '.')
-		return true;
-	if (input.find(".f")!=std::string::npos || input.find("-f")!=std::string::npos || input.find("-.")!=std::string::npos)
+	if (input.size() == 1 && (input[0] < '0' || input[0] > '9'))
 		return true;
 	return false;
 }
 
-bool is_pseudo(const std::string& input)
+bool is_number(const std::string &input)
 {
-	if (input == "nan" || input == "nanf" || input == "-inf" ||  input == "+inf" ||  input == "-inff" ||  input == "+inff")
+	bool is_minus = input[0] == '-';
+	if ((is_minus && input[1] == '.') || (!is_minus && input[0] == '.') || input[input.size() - 1] == '.' || (input[input.size() - 1] == 'f' && input[input.size() - 2] == '.'))
+		return false;
+	for (size_t i = is_minus ? 1 : 0; i < input.size() - 1; i++)
+	{
+		if ((input[i] < '0' || input[i] > '9') && input[i] != '.')
+			return false;
+	}
+	return true;
+}
+
+int dot_count(const std::string &input)
+{
+	int count = 0;
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		if (input[i] == '.')
+			count++;
+	}
+	return count;
+}
+
+bool is_int(const std::string &input)
+{
+	if (input[0] != '-' && (input[0] < '0' || input[0] > '9'))
+		return false;
+	for (size_t i = 1; i < input.size(); i++)
+	{
+		if (input[i] < '0' || input[i] > '9')
+			return false;
+	}
+	return true;
+}
+
+bool is_float(const std::string &input)
+{
+	bool end_is_f = input[input.size() - 1] == 'f';
+	int dots = dot_count(input);
+	bool is_num = is_number(input);
+
+	if (dots <= 1 && is_num && end_is_f)
+		return true;
+	return false;
+}
+
+bool is_double(const std::string &input)
+{
+	int dots = dot_count(input);
+	bool is_num = is_number(input);
+
+	if (dots <= 1 && is_num)
+		return true;
+	return false;
+}
+
+bool is_pseudo(const std::string &input)
+{
+	if (input == "nan" || input == "nanf" || input == "-inf" || input == "+inf" || input == "inf" || input == "-inff" || input == "+inff" || input == "inff")
 		return true;
 	return false;
 }
@@ -164,20 +269,4 @@ std::string get_integer(const std::string& input)
 		return input.substr(0, pos);
 	else
 		return input;
-}
-
-bool is_overflow(const std::string& input)
-{
-	std::string check_integer = get_integer(input);
-	double stk = 0;
-	bool is_minus = input[0] == '-' ? true : false;
-
-	int len = check_integer.size();
-	for (int i = is_minus ? 1 : 0; i < len; i++)
-		stk = (stk * 10) + (check_integer[i] - '0');
-	if (!is_minus && stk > 2147483647)
-		return true;
-	if (is_minus && stk > 2147483648)
-		return true;
-	return false;
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -3,6 +3,14 @@
 
 #include <iostream>
 #include <sstream>
+#include <float.h>
+
+#define TYPE_CHAR 0
+#define TYPE_INT 1
+#define TYPE_FLOAT 2
+#define TYPE_DOUBLE 3
+#define TYPE_PSEUDO 4
+#define TYPE_IMPOSSIBLE 5
 
 class ScalarConverter
 {
